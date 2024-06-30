999bbc28fdf05732dabfcd1575a2877943a7d187
Chamesss <chamsedin.azouz@gmail.com>
2024-06-30 23:47:53 +0100

    init header - footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ActiveSectionContextProvider from "@/context/active-section-context";
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +19,32 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="!scroll-smooth">
+      <meta name="author" content="ChamseDin Azouz" />
+      <meta property="og:title" content="ChamseDin Azouz | Portfolio" />
+      <meta
+        property="og:description "
+        content="I am a full-stack web developer. This portfolio showcases my work."
+      />
+      <meta property="og:url" content="https://chamsedin-azouz.vercel.app/" />
+      <meta
+        property="og:image"
+        content="https://chamsedin-azouz.vercel.app/_next/image?url=%2Fopengraph.png&w=640&q=95"
+      />
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
+      >
+        <div className="bg-[#fbe2e3] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]" />
+        <div className="bg-[#dbd7fb] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]" />
+        <div className="bg-[#dbd7fb] absolute -z-10 top-[50%] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-0 xl:right-20 2xl:right-20 dark:bg-[#3e3c5ab8]" />
+        <div className="bg-[#fbe2e3] absolute -z-10 bottom-0 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#a16b6c33]" />
+        <ActiveSectionContextProvider>
+          <Header />
+          {children}
+          <Footer />
+          <Toaster position="bottom-left" />
+        </ActiveSectionContextProvider>
+      </body>
     </html>
   );
 }
