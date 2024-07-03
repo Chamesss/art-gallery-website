d8413d61f618475221f0a59e8ba4948d95b6283c
Chamesss <chamsedin.azouz@gmail.com>
2024-07-03 14:46:33 +0100

    hero adj

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,7 +8,7 @@ import "swiper/css/effect-fade";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 
-import { EffectFade, Autoplay, Navigation } from "swiper/modules";
+import { EffectFade, Autoplay, Navigation, Pagination } from "swiper/modules";
 
 export default function Hero() {
   return (
@@ -17,7 +17,11 @@ export default function Hero() {
         spaceBetween={0}
         centeredSlides={true}
         effect={"fade"}
-        modules={[EffectFade, Autoplay, Navigation]}
+        modules={[EffectFade, Autoplay, Navigation, Pagination]}
+        pagination={{
+          clickable: true,
+          enabled: false,
+        }}
         className="mySwiper"
         navigation={{
           nextEl: ".swiper-button-next",
@@ -28,57 +32,88 @@ export default function Hero() {
           delay: 2500,
           disableOnInteraction: false,
         }}
+        loop={true}
       >
         <SwiperSlide>
           <div className="w-full h-[93vh] overflow-hidden relative">
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+              <div className="flex flex-row gap-16 items-center justify-center z-10">
+                <p className="w-[20rem] h-[20rem] text-wrap text-white text-center flex items-center ">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <Image
+                  width={1920}
+                  height={1080}
+                  priority={true}
+                  alt="model 1"
+                  src="https://swiperjs.com/demos/images/nature-1.jpg"
+                  className="w-[20rem] h-[20rem] object-fill z-10 rounded-md"
+                />
+              </div>
+            </div>
             <Image
               width={1920}
               height={1080}
               priority={true}
               alt="model 1"
               src="https://swiperjs.com/demos/images/nature-1.jpg"
-              className="w-[20rem] h-[20rem] object-fill absolute transform top-1/2 -translate-y-1/2 right-[20%] z-10 rounded-md"
-            />
-            <Image
-              width={1920}
-              height={1080}
-              priority={true}
-              alt="model 1"
-              src="https://swiperjs.com/demos/images/nature-1.jpg"
               className="w-full scale-110 h-full object-cover blur-xl -z-10"
             />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="w-full h-[93vh] overflow-hidden relative">
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+              <div className="flex flex-row gap-16 items-center justify-center z-10">
+                <p className="w-[20rem] h-[20rem] text-wrap text-white text-center flex items-center ">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <Image
+                  width={1920}
+                  height={1080}
+                  priority={true}
+                  alt="model 1"
+                  src="https://swiperjs.com/demos/images/nature-2.jpg"
+                  className="w-[20rem] h-[20rem] object-fill z-10 rounded-md"
+                />
+              </div>
+            </div>
             <Image
               width={1920}
               height={1080}
               priority={true}
               alt="model 1"
               src="https://swiperjs.com/demos/images/nature-2.jpg"
-              className="w-[20rem] h-[20rem] object-fill absolute transform top-1/2 -translate-y-1/2 right-[20%] z-10 rounded-md"
-            />
-            <Image
-              width={1920}
-              height={1080}
-              priority={true}
-              alt="model 1"
-              src="https://swiperjs.com/demos/images/nature-2.jpg"
               className="w-full scale-110 h-full object-cover blur-xl -z-10"
             />
           </div>
         </SwiperSlide>
         <SwiperSlide>
           <div className="w-full h-[93vh] overflow-hidden relative">
-            <Image
-              width={1920}
-              height={1080}
-              priority={true}
-              alt="model 1"
-              src="https://swiperjs.com/demos/images/nature-3.jpg"
-              className="w-[20rem] h-[20rem] object-fill absolute transform top-1/2 -translate-y-1/2 right-[20%] z-10 rounded-md"
-            />
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+              <div className="flex flex-row gap-16 items-center justify-center z-10">
+                <p className="w-[20rem] h-[20rem] text-wrap text-white text-center flex items-center ">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <Image
+                  width={1920}
+                  height={1080}
+                  priority={true}
+                  alt="model 1"
+                  src="https://swiperjs.com/demos/images/nature-3.jpg"
+                  className="w-[20rem] h-[20rem] object-fill z-10 rounded-md"
+                />
+              </div>
+            </div>
             <Image
               width={1920}
               height={1080}
@@ -91,14 +126,24 @@ export default function Hero() {
         </SwiperSlide>
         <SwiperSlide>
           <div className="w-full h-[93vh] overflow-hidden relative">
-            <Image
-              width={1920}
-              height={1080}
-              priority={true}
-              alt="model 1"
-              src="https://swiperjs.com/demos/images/nature-4.jpg"
-              className="w-[20rem] h-[20rem] object-fill absolute transform top-1/2 -translate-y-1/2 right-[20%] z-10 rounded-md"
-            />
+            <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10">
+              <div className="flex flex-row gap-16 items-center justify-center z-10">
+                <p className="w-[20rem] h-[20rem] text-wrap text-white text-center flex items-center ">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                  laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <Image
+                  width={1920}
+                  height={1080}
+                  priority={true}
+                  alt="model 1"
+                  src="https://swiperjs.com/demos/images/nature-4.jpg"
+                  className="w-[20rem] h-[20rem] object-fill z-10 rounded-md"
+                />
+              </div>
+            </div>
             <Image
               width={1920}
               height={1080}
