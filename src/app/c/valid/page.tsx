a80cc9e2adbcd0d17bf4bafbce52a2af0e4bc73f
Chamesss <chamsedin.azouz@gmail.com>
2024-07-15 01:56:10 +0100

    suspense wrapper

--- src/app/c/valid/page.tsx
+++ src/app/c/valid/page.tsx
@@ -1,8 +1,16 @@
 'use client'
 import { useSearchParams } from 'next/navigation'
-import React, { useState } from 'react'
+import React, { Suspense, useState } from 'react'
 
-export default function ValidateToken() {
+export default function ValidateTokenPage() {
+    return (
+        <Suspense fallback={'Loading...'}>
+            <ValidateToken />
+        </Suspense>
+    )
+}
+
+export function ValidateToken() {
     const searchParams = useSearchParams()
     const [token, setToken] = useState(() => searchParams.get('token') || '0')
     const [modelId, setModelId] = useState(() => searchParams.get('t') || '0')
