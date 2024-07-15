dca3924922c05dc9c8b2259aeda1bbcc4f144c8b
Chamesss <chamsedin.azouz@gmail.com>
2024-07-15 02:18:51 +0100

    checker

--- src/app/c/valid/components/validate-token.tsx
+++ src/app/c/valid/components/validate-token.tsx
@@ -1,4 +1,6 @@
 'use client'
+import { models } from '@/lib/token'
+import { firstModel } from '@/lib/token/first-model'
 import { useSearchParams } from 'next/navigation'
 import { useState } from 'react'
 
@@ -6,9 +8,31 @@ export default function ValidateToken() {
     const searchParams = useSearchParams()
     const [token, setToken] = useState(() => searchParams.get('token') || '0')
     const [modelId, setModelId] = useState(() => searchParams.get('t') || '0')
+
+    let exists: boolean = false
+    const model = models.find((m) => m.id === modelId)
+    if (model) {
+        exists = model.table.includes(token)
+    }
+
+    if (!exists) {
+        return (
+            <div className="flex w-full justify-center">
+                <div className="max-w-[80rem] space-y-8 p-4 text-center">
+                    <p>Doesn&apos;t exists.. :(</p>
+                </div>
+            </div>
+        )
+    }
+
     return (
-        <div>
-            Token === {token} modelId === {modelId}
+        <div className="flex w-full justify-center">
+            <div className="max-w-[80rem] space-y-8 p-4 text-center">
+                <p>Exists !!</p>
+                <p>
+                    Token === {token} modelId === {modelId}
+                </p>
+            </div>
         </div>
     )
 }
---
+++ src/lib/token/index.ts
@@ -0,0 +1,5 @@
+import { firstModel } from "./first-model";
+import { secondModel } from "./second-model";
+import { thirdModel } from "./third-model";
+
+export const models = [firstModel, secondModel, thirdModel]
