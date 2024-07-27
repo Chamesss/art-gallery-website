1675e30138983b5d1607f6545e2cdb697f1eb33a
Chamesss <chamsedin.azouz@gmail.com>
2024-07-27 01:09:53 +0100

    env type update

--- next.config.mjs
+++ next.config.mjs
@@ -11,8 +11,10 @@ const nextConfig = {
         ],
     },
     env: {
-        RECAPTCHA_APP_KEY: '6LfKTxkqAAAAAJK445DH1zok6bccSfSxBl9Lq94X',
-        RECAPTCHA_SECRET_KEY: '6LfKTxkqAAAAACTd_lMagmnDmL6r7VQxxGomIj8z',
+        // RECAPTCHA_APP_KEY: '6LfKTxkqAAAAAJK445DH1zok6bccSfSxBl9Lq94X',
+        // RECAPTCHA_SECRET_KEY: '6LfKTxkqAAAAACTd_lMagmnDmL6r7VQxxGomIj8z',
+        RECAPTCHA_APP_KEY: '6Le4UhkqAAAAANsaY7xDxgl6L2YqNxEk4v7Zk3cm',
+        RECAPTCHA_SECRET_KEY: '6Le4UhkqAAAAAMsaGtPh3AEelITZSCsMEcOVmEph',
     },
 }
 
